23a62590e51d2a58c8e5a57ec0224bc27c204c1d
EDU <0narara0@gmail.com>
2018-10-25 13:36:44 +0900

    add onlongclicklistener

--- app/src/main/java/com/cmy/www/tryeventcodeimplements/MainActivity.java
+++ app/src/main/java/com/cmy/www/tryeventcodeimplements/MainActivity.java
@@ -6,17 +6,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
-    Button btn1, btn2;
+    Button btn1, btn2, btn3, btn4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1 = (Button)findViewById(R.id.btn1);
+        btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(this);
-        btn2 = (Button)findViewById(R.id.btn2);
+        btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(this);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnLongClickListener(this);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn4.setOnLongClickListener(this);
 
     }
 
@@ -28,15 +33,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
         switch (id) {
             case R.id.btn1:
-                Toast.makeText(this,((Button)v).getText(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn2:
-                Toast.makeText(this,((Button)v).getText(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
                 break;
-
 
 
         }
 
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn3:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn4:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return false;
     }
 }
